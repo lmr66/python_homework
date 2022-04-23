61c191bc498a9d11642f2e823ff1ffc151599327
lixinhan <295709194@qq.com>
2022-04-23 20:34:13 +0800

    Print something before the for item-work2

--- python_homework/1forList.py
+++ python_homework/1forList.py
@@ -10,5 +10,10 @@ for player in players:
 
 players=['a','b','c']
 for player in players:
-    print(player.title()+" think this is a good game")
+    print(player.title()+" think this is a good game\n")
 
+players=['a','b','c']
+for player in players:
+    print(player.title()+" think this is a good game\n")
+
+print("They all engoyed the game very much")
